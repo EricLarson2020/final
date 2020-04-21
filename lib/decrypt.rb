400113dd566c1c7d700071d70e85220b71f8512b
Eric Larson <ericlarsonbroom@gmail.com>
2020-04-20 21:38:18 -0600

    Created command line outputs for encrypt

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -12,3 +12,5 @@ decrypted_message = enigma.decrypt(encrypted_message, ARGV[2], ARGV[3])
 writer = File.open(ARGV[1], "w")
 writer.write(decrypted_message[:decryption])
 writer.close
+
+p "Created #{(ARGV[1])} with the key #{decrypted_message[:key]} and date #{decrypted_message[:date]}"
